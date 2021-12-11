8feaa14ca5643ecee57559ea6a235b8018b9cf6b
tracy <734854177@qq.com>
2021-12-11 21:31:47 +0800

    3rd version

--- src/main/java/com/wzh/git/GitDemo01.java
+++ src/main/java/com/wzh/git/GitDemo01.java
@@ -5,5 +5,6 @@ public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("GitDemo01");
         System.out.println("push 1111112");
+        System.out.println("push 2222222");
     }
 }
